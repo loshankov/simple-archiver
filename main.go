67f66e7473bfbed52e371491df66729c3ef4b674
Nikita Loshankov <loshankov.n@gmail.com>
2026-07-27 09:31:14 +0300

    add NewArchive constructor

--- main.go
+++ main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+type SimpleArchiver struct {
+	inputPath  string
+	outputPath string
+	buffer     []byte
+}
+
+func NewArchiver(inputPath string) *SimpleArchiver {
+	return &SimpleArchiver{
+		inputPath: inputPath,
+		buffer:    make([]byte, 1024*8),
+	}
+}
+
 func main() {
-	fmt.Println("Простой архиватор запущен")
+	sa := NewArchiver("input.txt")
+	fmt.Printf("Архиватор создан, размер буфера: %d байт\nпуть к исходному файлу: %s", cap(sa.buffer), sa.inputPath)
 }
