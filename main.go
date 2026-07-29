8b3ddd56749be83e2773e5b1f7548b3083d90ef3
Nikita Loshankov <loshankov.n@gmail.com>
2026-07-29 09:31:29 +0300

    add compressEmpty

--- main.go
+++ main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 type SimpleArchiver struct {
 	inputPath  string
 	outputPath string
@@ -26,17 +22,33 @@ func (sa *SimpleArchiver) compressEmpty(data []byte) []byte {
 
 func (sa *SimpleArchiver) countRepeating(data []byte) []byte {
 	data = sa.compressEmpty(data)
-
-	symbolsMap := make(map[string]int)
-	for _, symbol := range data {
-		symbolsMap[string(symbol)] += 1
+	if len(data) == 0 {
+		return data
 	}
 
-	return data
+	currentChar := data[0]
+	count := 1
+	var result []byte
+
+	for i := 1; i < len(data); i++ {
+		if currentChar == data[i] {
+			count++
+		} else {
+			result = append(result, byte(count), currentChar)
+			currentChar = data[i]
+			count = 1
+		}
+
+	}
+
+	result = append(result, byte(count), currentChar)
+
+	return result
 }
 
 func main() {
 	sa := NewArchiver("input.txt")
-	fmt.Printf("Архиватор создан, размер буфера: %d байт\nпуть к исходному файлу: %s", cap(sa.buffer), sa.inputPath)
+	//fmt.Printf("Архиватор создан, размер буфера: %d байт\nпуть к исходному файлу: %s", cap(sa.buffer), sa.inputPath)
 	sa.countRepeating([]byte("AABBB"))
+	sa.countRepeating([]byte("AABAB"))
 }
