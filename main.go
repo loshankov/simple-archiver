15a79b54951311f6cf8bb190397f5c19382f5687
nikita.loshankov <nikita.loshankov@im.systems>
2026-09-15 08:52:54 +0300

    refactor updateInput

--- main.go
+++ main.go
@@ -445,12 +445,13 @@ func (m model) updateInput(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
 			if err := arch.CompressFile(m.inputPath, m.inputPath+".sarch"); err != nil {
 				m.err = err
 			}
+			m.state = "menu"
 		case "decompress":
 			if err := arch.DecompressFile(m.inputPath, filepath.Dir(m.inputPath)); err != nil {
 				m.err = err
 			}
+			m.state = "menu"
 		}
-		m.state = "menu"
 	case "backspace":
 		if len(m.inputPath) > 0 {
 			m.inputPath = string([]rune(m.inputPath)[0 : len([]rune(m.inputPath))-1])
